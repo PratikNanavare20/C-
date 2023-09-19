86f5a217383956e9287018e31f412dabce6de0e9
PratikNanavare20 <121279927+PratikNanavare20@users.noreply.github.com>
2023-09-19 12:42:12 +0530

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 123;
+            float f1 = 12.76f;
+            decimal d1 = 34.6m;
+            double d2 = 100372.3047d;
+            Boolean b1 = true;
+
+            Console.WriteLine(i1);
+            Console.WriteLine(f1);
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(b1);
+
+            Console.WriteLine("The type of the variables");
+            Console.WriteLine(i1.GetType());
+            Console.WriteLine(f1.GetType());
+            Console.WriteLine(d1.GetType());
+            Console.WriteLine(d2.GetType());
+            Console.WriteLine(b1.GetType());
+        }
+    }
+}
