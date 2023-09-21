1a4ad87620e03fecf07fdc21b95fb66d8fe0c7cd
PratikNanavare20 <121279927+PratikNanavare20@users.noreply.github.com>
2023-09-21 15:29:13 +0530

    Add files via upload

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime d1 = new DateTime();
+            Console.WriteLine(d1);
+
+            DateTime currentdatetime = DateTime.Now;
+            Console.WriteLine(currentdatetime);
+
+            DateTime todayDate = 
+        }
+    }
+}
