fc74eb6e11cc03c0a772f4736f1bb345ca440cd0
PratikNanavare20 <121279927+PratikNanavare20@users.noreply.github.com>
2023-10-03 18:36:48 +0530

    Add files via upload

---
+++ Constructor and Dustructor/Constructor and Dustructor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_and_Dustructor
+{
+    public class Constructor
+    {
+        public  Constructor()
+        {
+            Console.WriteLine("This is a default Constructor");
+        }
+    }
+
+    public class ParaConst
+    {
+        int id;
+        string name;
+        public ParaConst(int i, string n)
+        {
+            id = i;
+            name = n;
+        }
+        public void display()
+        {
+            Console.WriteLine(id+" "+name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Constructor c = new Constructor();
+
+            ParaConst p = new ParaConst(2, "Pratik");
+            p.display();
+        }
+    }
+}
