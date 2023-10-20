0458aa2ad82e55b0dd170e0b6095feb58ed6b181
PratikNanavare20 <121279927+PratikNanavare20@users.noreply.github.com>
2023-10-20 18:28:24 +0530

    Add files via upload

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    public class Program
+    {
+        ///********************************
+        public Nullable<int> i = null;
+        
+        ///********************
+        public int? a = 20;
+        public int b = 10;
+
+        static void Main(string[] args)
+        {    
+            Program p = new Program();
+            
+            //**************************
+            if(p.i == null)
+            {
+                Console.WriteLine("NULL");
+            }
+            else
+            {
+                Console.WriteLine(p.i);
+            }
+
+            //***************************
+            if (p.a == null)
+            {
+                Console.WriteLine("The Value of 'a' is NULL");
+            }
+            else
+            {
+                Console.WriteLine("Addition of the elements is "+(p.a + p.b));
+                Console.WriteLine("Substraction of the elements is " + (p.a - p.b));
+                Console.WriteLine("Multiplication of the elements is " + (p.a * p.b));
+                Console.WriteLine("Division of the elements is " + (p.a / p.b));
+            }
+        }
+    }
+   
+}
